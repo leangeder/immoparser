9ecfb7bd94653c6252b7b5bcf640035cef69969a
leangeder <leangeder@hotmail.com>
2019-04-25 06:31:50 +0100

    bck

--- pkg/sites/sites.go
+++ pkg/sites/sites.go
@@ -4,6 +4,20 @@ type Annonces interface {
 	GetAnnonces() string
 }
 
-func Get(a Annonces) string {
-	return a.GetAnnonces()
+// func Get(a Annonces) string {
+// 	//  listAnnonceleboncoin := leboncoin.Annonce{}
+// 	// listAnnoncedomimmo := domimmo.Annonce{}
+//
+// 	// log.Println(sites.Get(listAnnonceleboncoin))
+// 	// log.Println(sites.Get(listAnnoncedomimmo))
+// 	return a.GetAnnonces()
+// }
+
+type site struct{}
+
+func GetAnnoncesFromSites(a Annonces) string {
+}
+
+func Get() string {
+	return GetAnnoncesFromSites(&site{})
 }
